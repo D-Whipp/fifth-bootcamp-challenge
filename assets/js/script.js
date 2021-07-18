bc9634d7443f22783bf4d76645d8c584b16bce13
Mr Whipp <dwhipp88@gmail.com>
2021-07-18 17:58:15 -0600

    about to refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
 var currentDayEl = document.querySelector("#currentDay");
+var timeEl = document.querySelector(".time").innerText;
+console.log(nineTimeEl);
 
 var nowDate = moment().format("MMMM Do YYYY");
 var nowTime = moment().format("hh:mm:ss A");
+var compareTime = moment().format("hA");
 
 console.log(nowDate);
 console.log(nowTime);
+console.log(compareTime);
 
 var setCurrentDay = function () {
   var setDate = (currentDayEl.innerText = nowDate);
   return setDate;
 };
 
+var checkTime = function (nowTime) {
+  var currentTime = nowTime;
+
+  for (i = 0; i < 8; i++) {}
+
+  console.log(currentTime);
+  console.log("hi");
+};
+
+var convertTime = function (value) {
+
+};
+
+convertTime();
+checkTime(nowTime);
 setCurrentDay();
