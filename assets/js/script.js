472e1617d493c4af2cf027f8b815718d91bf9d08
Mr Whipp <dwhipp88@gmail.com>
2021-07-18 19:19:52 -0600

    convert javascript to jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-var currentDayEl = document.querySelector("#currentDay");
 var timeEl = document.querySelector(".timeblock .time").innerText;
-console.log(timeEl);
 
 var nowDate = moment().format("MMMM Do YYYY");
 var nowTime = moment().format("hh:mm:ss A");
@@ -11,12 +9,5 @@ console.log(nowDate);
 console.log(nowTime);
 console.log(compareTime);
 
-var setCurrentDay = function () {
-  var setDate = (currentDayEl.innerText = nowDate);
-  return setDate;
-};
 
-
-var convertTime = function (value) {};
-
-setCurrentDay();
+$("#currentDay").html(nowDate);
