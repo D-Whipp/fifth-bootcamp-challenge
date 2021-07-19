71c87354dea073f1488eeee0c90fcbfb0df08ab3
Mr Whipp <dwhipp88@gmail.com>
2021-07-18 20:14:39 -0600

    timeblock iteration failing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,19 @@ var compareTime = function () {
   // loop through timeblock elements
   $(".timeblock").each(function () {
     var timeBlocks = parseInt($(this).attr("id").split("-")[1]);
-    console.log(timeBlocks);
     //   check if the time is in the past
     if (timeBlocks < nowTime) {
         $(textboxEl).addClass("past");
+    } else if (timeBlocks === nowTime) {
+        console.log(timeBlocks);
+        // check if the time is now
+        $(textboxEl).removeClass("past");
+        $(textboxEl).addClass("present");
+    } else {
+        // check if the time is in the future
+        $(textboxEl).removeClass("past");
+        $(textboxEl).removeClass("present");
+        $(textboxEl).addClass("future");
     }
   });
 };
