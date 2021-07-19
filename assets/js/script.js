de3eb817daa342a41498f9d57ddd16d385f73cf3
Mr Whipp <dwhipp88@gmail.com>
2021-07-18 19:40:10 -0600

    looping through divs and console logging results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-var timeEl = document.querySelector(".timeblock .time").innerText;
-
 var nowDate = moment().format("MMMM Do YYYY");
-var nowTime = moment().format("hh:mm:ss A");
-var compareTime = moment().format("hA");
-
-console.log(moment().hour());
-console.log(nowDate);
-console.log(nowTime);
-console.log(compareTime);
-
-
 $("#currentDay").html(nowDate);
+
+var compareTime = function () {
+  var nowTime = moment().format("HH");
+  console.log(nowTime);
+
+  // loop through timeblock elements
+  $(".timeblock").each(function () {
+    console.log(parseInt($(this).attr("id").split("-")[1]));
+  });
+  console.log("I DID IT!!!!!!!!!!!!!!!!!!!");
+};
+
+compareTime();
