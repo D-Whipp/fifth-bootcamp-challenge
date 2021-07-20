3fa8046051b8cadecc8f0b05e84d9cc20d8b04f3
Mr Whipp <dwhipp88@gmail.com>
2021-07-19 23:02:07 -0600

    targeting children div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,41 @@
 var textboxEl = document.querySelector("#text");
+var nowTime = +moment().format("HH");
+var fixedTime = +moment().hour(13).format("HH");
+console.log(fixedTime);
 
+// sets current day on top of page
 var nowDate = moment().format("MMMM Do YYYY");
 $("#currentDay").html(nowDate);
 
+$(".timeblock").each(function (i, item) {
+  console.log($(this).children("#text").addClass("past"));
+});
 
 var compareTime = function () {
-  var nowTime = +moment().format("HH");
-  console.log(typeof nowTime);
-
-  // loop through timeblock elements
-  $(".timeblock").each(function () {
-    var timeBlocks = parseInt($(this).attr("id").split("-")[1]);
-    //   check if the time is in the past
-    if (timeBlocks < nowTime) {
-        $(textboxEl).addClass("past");
-    } else if (timeBlocks === nowTime) {
-        console.log(timeBlocks);
-        // check if the time is now
-        $(textboxEl).removeClass("past");
-        $(textboxEl).addClass("present");
-    } else {
-        // check if the time is in the future
-        $(textboxEl).removeClass("past");
-        $(textboxEl).removeClass("present");
-        $(textboxEl).addClass("future");
-    }
-  });
+//   console.log(nowTime);
+//   //   loop through timeblock elements
+//   $(".timeblock").each(function (i, item) {
+//     console.log($(".text-box"));
+//     var eachBlock = parseInt($(item).attr("id").split("-")[1]);
+//     console.log(item);
+//   });
 };
+
+// var auditTime = function (eachBlock) {
+//     var a = eachBlock;
+//   if (a < nowTime) {
+//     $(textboxEl).addClass("past");
+//   } else if (a === nowTime) {
+//     // check if the time is now
+//     $(textboxEl).removeClass("past");
+//     $(textboxEl).addClass("present");
+//   } else {
+//     // check if the time is in the future
+//     $(textboxEl).removeClass("past");
+//     $(textboxEl).removeClass("present");
+//     $(textboxEl).addClass("future");
+//     console.log("hi");
+//   }
+// };
 
 compareTime();
