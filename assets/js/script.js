091b6d1994168d6f349b87761500f99da17ff3f7
Mr Whipp <dwhipp88@gmail.com>
2021-07-20 01:21:35 -0600

    using classList method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,41 @@
 var textboxEl = document.querySelector("#text");
 var nowTime = +moment().format("HH");
 var fixedTime = +moment().hour(13).format("HH");
-console.log(fixedTime);
 
 // sets current day on top of page
 var nowDate = moment().format("MMMM Do YYYY");
 $("#currentDay").html(nowDate);
 
-$(".timeblock").each(function (i, item) {
-  console.log($(this).children("#text").addClass("past"));
-});
+// $(".timeblock").each(function (i, item) {
+//   console.log($(this).children("#text").addClass("past"));
+// });
 
 var compareTime = function () {
-//   console.log(nowTime);
-//   //   loop through timeblock elements
-//   $(".timeblock").each(function (i, item) {
-//     console.log($(".text-box"));
-//     var eachBlock = parseInt($(item).attr("id").split("-")[1]);
-//     console.log(item);
-//   });
+  //   loop through timeblock elements
+  $(".timeblock").each(function (i, item) {
+    var eachBlock = $(this).children("#text");
+    var eBlock = document.querySelector(".timeblock .text-box");
+    console.log(eBlock);
+    console.log(this);
+    console.log(eachBlock);
+
+    if (eachBlock < nowTime) {
+      eBlock.classList.add("past");
+    } else if (eachBlock === nowTime) {
+      // check if the time is now
+      eBlock.classList.remove("past");
+      eBlock.classList.add("present");
+    } else {
+      // check if the time is in the future
+      eBlock.classList.remove("past");
+      eBlock.classList.remove("present");
+      eBlock.classList.add("future");
+    }
+  });
 };
 
 // var auditTime = function (eachBlock) {
 //     var a = eachBlock;
-//   if (a < nowTime) {
-//     $(textboxEl).addClass("past");
-//   } else if (a === nowTime) {
-//     // check if the time is now
-//     $(textboxEl).removeClass("past");
-//     $(textboxEl).addClass("present");
-//   } else {
-//     // check if the time is in the future
-//     $(textboxEl).removeClass("past");
-//     $(textboxEl).removeClass("present");
-//     $(textboxEl).addClass("future");
-//     console.log("hi");
-//   }
 // };
 
 compareTime();
