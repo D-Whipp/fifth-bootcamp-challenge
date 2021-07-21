c01c62b15bd3da389341e9242f020828e68beac4
Mr Whipp <dwhipp88@gmail.com>
2021-07-20 19:12:54 -0600

    working on auditing TIme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,22 @@
-var textboxEl = document.querySelector("#text");
-var nowTime = +moment().format("HH");
-var fixedTime = +moment().hour(13).format("HH");
-
-// sets current day on top of page
+// sets current date at top of page
 var nowDate = moment().format("MMMM Do YYYY");
 $("#currentDay").html(nowDate);
 
-// $(".timeblock").each(function (i, item) {
-//   console.log($(this).children("#text").addClass("past"));
-// });
+var timeBlockEl = $(".timeblock");
 
-var compareTime = function () {
-  //   loop through timeblock elements
-  $(".timeblock").each(function (i, item) {
-    var eachBlock = $(this).children("#text");
-    var eBlock = document.querySelector(".timeblock .text-box");
-    console.log(eBlock);
-    console.log(this);
-    console.log(eachBlock);
+var auditTime = function (timeBlockEl) {
+  var currentTime = moment().format("HH");
+  console.log(currentTime);
 
-    if (eachBlock < nowTime) {
-      eBlock.classList.add("past");
-    } else if (eachBlock === nowTime) {
-      // check if the time is now
-      eBlock.classList.remove("past");
-      eBlock.classList.add("present");
-    } else {
-      // check if the time is in the future
-      eBlock.classList.remove("past");
-      eBlock.classList.remove("present");
-      eBlock.classList.add("future");
+  $(".timeblock").each(function () {
+    var timeBlocks = parseInt($(this).attr("id").split("-")[1]);
+    var textAreaEL = document.querySelector("#text");
+    console.log(textAreaEL);
+
+    if (timeBlocks < currentTime) {
+      textAreaEL.addClass("past");
     }
   });
 };
 
-compareTime();
+auditTime();
